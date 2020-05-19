f82d8cd2f2773190c8904d3429e247829151b40d
MIchiel1234 <mvdmerwe9245@gmail.com>
2020-05-19 01:02:41 -0700

    Add files via upload

---
+++ datagenaration/datagenaration/filehandeler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace datagenaration
+{
+    class filehandeler
+    {
+        public void write(int moneyspend, int time, int location)
+        {
+            FileStream fs = new FileStream("demo.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(moneyspend+","+ time+","+location);
+            sw.Close();
+            fs.Dispose();
+        }
+    }
+}
